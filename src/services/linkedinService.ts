dc978e7060fefb1ab61d712cf37c18daa3bdb61f
JakubUherDev <jjuherdev@gmail.com>
2024-05-03 22:42:48 +0100

    🔧 add: change to ts

--- src/services/linkedinService.ts
+++ src/services/linkedinService.ts
@@ -1,16 +1,38 @@
 import * as dotenv from 'dotenv';
 import cheerio from "cheerio";
+import puppeteerCore from "puppeteer-core";
 import puppeteer from "puppeteer";
+import chromium from "@sparticuz/chromium";
+
+
+export const dynamic = "force-dynamic";
 
 dotenv.config();
 
 export class LinkedinService {
     private baseLinkedinProfile = `https://www.linkedin.com/in`
 
+    public async getBrowser() {
+        if (process.env.VERCEL_ENV === "production") {
+            const executablePath = await chromium.executablePath();
+
+            const browser = await puppeteerCore.launch({
+                args: chromium.args,
+                defaultViewport: chromium.defaultViewport,
+                executablePath,
+                headless: chromium.headless === true ? chromium.headless : undefined,
+            });
+            return browser;
+        } else {
+            const browser = await puppeteer.launch();
+            return browser;
+        }
+    }
+
     public async fetchLinkedinProfile(username: any) {
         try {
             // Launch a headless browser
-            const browser = await puppeteer.launch();
+            const browser = await this.getBrowser()
             const page = await browser.newPage();
 
             // Navigate to the URL
