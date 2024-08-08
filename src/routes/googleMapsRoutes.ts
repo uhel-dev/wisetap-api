8332665b7755d0dc567b00bcd410298a5a12c588
JakubUherDev <jjuherdev@gmail.com>
2024-08-08 21:49:22 +0100

    feat: fetch also with placeid

--- src/routes/googleMapsRoutes.ts
+++ src/routes/googleMapsRoutes.ts
@@ -7,6 +7,7 @@ const googleMapsService = new GoogleMapsService();
 const googleMapsController = new GoogleMapsController(googleMapsService);
 
 router.get('/find', googleMapsController.findPlaces);
+router.get('/find_v2', googleMapsController.findPlacesV2);
 router.get('/photo', googleMapsController.fetchGoogleProfilePhoto);
 
 export default router;
